18b37d4371694b467425f93bc5bfa857c68da530
munzi-Kim <113533226+munzi-Kim@users.noreply.github.com>
2022-11-21 21:17:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const app = require("express")();
 const httpServer = require("http").createServer(app);
 const server = require("socket.io")(httpServer);
 
+app.get("/", function site_mainPage(request, result) {
+    result.sendFile("html/main.html");
+});
+
+app.get("/info", function site_basicInfo(request, result) {
+    result.sendFile("html/info.html");
+});
+
 server.on("connection", function server_connect(socket){
+    socket.emit("event");
+
     socket.on("message", function server_getMessage(message){
         console.log("MESSAGE - " + message);
 
